cf9a734e14de2a5a13357645922863f0303551bd
rauldmj3 <r.mingo@sapalomera.cat>
2024-04-07 21:08:26 +0200

    articles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,26 +30,26 @@
                     @endauth
                 </div>
             @endif
-
-            <div style="margin-top: 3%;margin-bot:5%;margin-left: 10%;margin-right:10%;">
-                <h1 class="text-4xl font-bold tracking-tight text-black sm:text-6xl" style="padding-bottom:5%">Artículos</h1>
-            @foreach($article as $articleItem)
-                <ul role="list" class="divide-y divide-gray-100">
-                    <li class="flex justify-between gap-x-6 py-5">
-                    <div class="flex min-w-0 gap-x-4">
-                        <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-gray-900"><strong>{{ $articleItem->titol }}</strong></p>
-                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $articleItem->descripcio }}</p>
+                
+            <div>
+                <h1 class="text-4xl font-bold tracking-tight text-black sm:text-6xl" style="padding-bottom:5%; color: white; font-size: 50px">Artículos</h1>
+                @foreach($article as $articleItem)
+                    <ul role="list" class="divide-y divide-gray-100">
+                        <li class="flex justify-between gap-x-6 py-5">
+                        <div class="flex min-w-0 gap-x-4">
+                            <div class="min-w-0 flex-auto">
+                            <p class="text-sm font-semibold leading-6 text-gray-900"><strong style="color: white">{{ $articleItem->titol }}</strong></p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $articleItem->descripcio }}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <p class="text-sm leading-6 text-gray-900"><a href="">{{ $articleItem->titol }}</a></p>
-                        <p class="mt-1 text-xs leading-5 text-gray-500">Última modificación<time datetime="{{ $articleItem->created_at}}">{{ $articleItem->created_at->diffForHumans() }}</time></p>
-                    </div>
-                    </li>
-                </ul>
-            
-            @endforeach
+                        <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                            <p class="text-sm leading-6 text-gray-900"><a href="">{{ $articleItem->titol }}</a></p>
+                            <p class="mt-1 text-xs leading-5 text-gray-500">{{ $articleItem->descripcio }}</p>
+                        </div>
+                        </li>
+                    </ul>
+                
+                @endforeach
             {{ $article->links() }}
             </div>
         </div>
